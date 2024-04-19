119c4122fb711f160702a3af3c5aadceb2ed12eb
JGirCo <juanmanuelgiraldoc@gmail.com>
2024-04-18 21:54:28 -0500

    Restructured colors

---
+++ colors/gruvbox-dark-medium.nix
@@ -0,0 +1,24 @@
+{
+    base = "#282828";
+    text = "#282828";
+    text2 = "#ebdbb2";
+    inactive = "#458588";
+    focused = "#83a598";
+    alt = "#f38019";
+    urgent = "#fb4934";
+    binding = "#d3869b";
+
+    red = "#fb4934";
+    yellow = "#fabd2f";
+    pink = "#d3869b";
+    green = "#b8bb26";
+    blue = "#83a598";
+    purple = "#d3869b";
+    orange = "#f38019";
+
+    key = {
+      wezterm = "Gruvbox Dark (Gogh)";
+      nvim = "gruvbox";
+      isNvimBuiltin = true;
+    };
+}
---
+++ colors/rose-pine-dawn.nix
@@ -0,0 +1,24 @@
+{
+    base = "#faf4ed";
+    text = "#faf4ed";
+    text2 = "#575279";
+    inactive = "#d7827e";
+    focused = "#b4637a";
+    alt = "#56949f";
+    urgent = "#ea9d34";
+    binding = "#907aa9";
+
+    red = "#b4637a";
+    yellow = "#f6c177";
+    pink = "#d7827e";
+    green = "#56949f";
+    blue = "#286983";
+    purple = "#907aa9";
+    orange = "#ea9d34";
+
+    key = {
+      wezterm = "rose-pine-dawn";
+      nvim = "rose-pine";
+      isNvimBuiltin = true;
+    };
+}
