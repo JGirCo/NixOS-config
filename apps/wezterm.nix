438db01c9e55f90e86a07374061426a7c4709c0b
JGirCo <juanmanuelgiraldoc@gmail.com>
2024-04-27 14:22:55 -0500

    Reversed wezterm transparency changes

--- apps/wezterm.nix
+++ apps/wezterm.nix
@@ -1,41 +1,39 @@
 { config, lib, pkgs, theme, font, ... }:
 
-let
-  color_scheme = import ../colors.nix;
-in
-with color_scheme.${theme};{
-    programs.wezterm = {
-      enable = true;
-      enableZshIntegration = true;
-      extraConfig = ''
+let color_scheme = import ../colors.nix;
+in with color_scheme.${theme}; {
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = true;
+    extraConfig = ''
 
-        -- This table will hold the configuration.
-        local config = {}
+      -- This table will hold the configuration.
+      local config = {}
 
-        -- In newer versions of wezterm, use the config_builder which will
-        -- help provide clearer error messages
-        if wezterm.config_builder then
-          config = wezterm.config_builder()
-        end
+      -- In newer versions of wezterm, use the config_builder which will
+      -- help provide clearer error messages
+      if wezterm.config_builder then
+        config = wezterm.config_builder()
+      end
 
-        -- This is where you actually apply your config choices
+      -- This is where you actually apply your config choices
 
-        -- For example, changing the color scheme:
-        config.color_scheme = "${key.wezterm}"
-        config.font = wezterm.font '${font} Nerd Font'
-        config.hide_tab_bar_if_only_one_tab = true
-        config.audible_bell="Disabled"
-        config.window_background_opacity = 0.60
-        -- config.text_background_opacity = 0.0
+      -- For example, changing the color scheme:
+      config.color_scheme = "${key.wezterm}"
+      config.font = wezterm.font '${font} Nerd Font'
+      config.hide_tab_bar_if_only_one_tab = true
+      config.audible_bell="Disabled"
+      -- config.window_background_opacity = 0.60
+      -- config.text_background_opacity = 0.0
 
-        config.window_padding = {
-          left = 0,
-          right = 0,
-          top = 0,
-          bottom = 0,
-        }
-        -- and finally, return the configuration to wezterm
-        return config
-        '';
-    };
+      config.window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+      }
+      -- and finally, return the configuration to wezterm
+      return config
+    '';
+  };
 }
--- desktop/sway.nix
+++ desktop/sway.nix
@@ -18,171 +18,188 @@ let
   # active = "#${config.colorScheme.palette.base0A}";
   # urgent = "#${config.colorScheme.palette.base08}";
   # binding = "#${config.colorScheme.palette.base0E}";
-in
-with colorScheme."${theme}";
-{
-    imports = [
-      ./waybar.nix
-    ];
-    home.packages = with pkgs; [
-      swaybg
-      waybar
-      dunst
-      wofi
-      swayfx
-      grim
-      slurp
-      imagemagick
-      swappy
-      wl-clipboard
-    ];
-    # wayland.windowManager.swayfx.enable = true;
-    wayland.windowManager.sway = {
-      enable = true;
-      package = pkgs.swayfx;
-      extraConfig = ''
-        for_window [class = "^Emacs$"] opacity 0.85
-        # for_window [app_id = "^org.wezfurlong.wezterm"] opacity 0.75
-        blur enable
-        blur_radius 2
-        corner_radius 10
-        '';
-      config = {
-        terminal = "wezterm";
-        modifier = mod;
-        defaultWorkspace = "1";
-        workspaceAutoBackAndForth = true;
+in with colorScheme."${theme}"; {
+  imports = [ ./waybar.nix ];
+  home.packages = with pkgs; [
+    swaybg
+    waybar
+    dunst
+    wofi
+    swayfx
+    grim
+    slurp
+    imagemagick
+    swappy
+    wl-clipboard
+  ];
+  # wayland.windowManager.swayfx.enable = true;
+  wayland.windowManager.sway = {
+    enable = true;
+    package = pkgs.swayfx;
+    extraConfig = ''
+      for_window [class = "^Emacs$"] opacity 0.85
+      for_window [app_id = "^org.wezfurlong.wezterm"] opacity 0.75
+      blur enable
+      blur_radius 2
+      corner_radius 10
+    '';
+    config = {
+      terminal = "wezterm";
+      modifier = mod;
+      defaultWorkspace = "1";
+      workspaceAutoBackAndForth = true;
 
-          input = {
-            "*" = {
-              xkb_layout = "latam";
-              tap = "enabled";
-              natural_scroll = "enabled";
-            };
-
+      input = {
+        "*" = {
+          xkb_layout = "latam";
+          tap = "enabled";
+          natural_scroll = "enabled";
         };
+        "5215:711:Trust_Wireless_Mouse" = { natural_scroll = "disabled"; };
 
-        assigns = {"10" = [{class = "^Spotify$";}];};
+      };
 
-        fonts = { names = [ "${font} Nerd Font" ]; size = 9.0; };
-        startup = [
-          { command = "--no-startup-id pkill swaybg"; always = true; }
-          { command = "--no-startup-id swaybg -i ~/Pictures/wallpapers/${theme}.jpg"; always = true; }
+      assigns = { "10" = [{ class = "^Spotify$"; }]; };
+
+      fonts = {
+        names = [ "${font} Nerd Font" ];
+        size = 9.0;
+      };
+      startup = [
+        {
+          command = "--no-startup-id pkill swaybg";
+          always = true;
+        }
+        {
+          command =
+            "--no-startup-id swaybg -i ~/Pictures/wallpapers/${theme}.jpg";
+          always = true;
+        }
         #     command = "${pkgs.waybar}/bin/waybar";
-          { command = "--no-startup-id ${pkgs.waybar}/bin/waybar"; always = true; }
-          { command = "--no-startup-id nm-applet"; always = true; }
-        ];
-        window = {
-          border = 4;
-          titlebar = false;
+        {
+          command = "--no-startup-id ${pkgs.waybar}/bin/waybar";
+          always = true;
+        }
+        {
+          command = "--no-startup-id nm-applet";
+          always = true;
+        }
+      ];
+      window = {
+        border = 4;
+        titlebar = false;
+      };
+      gaps.inner = 5;
+
+      focus.followMouse = false;
+
+      keybindings = lib.mkOptionDefault {
+        "${mod}+q" = "kill";
+        "${mod}+b" = "exec firefox-beta";
+        "${mod}+d" = "exec wofi --show drun";
+        "${mod}+t" = "exec wezterm";
+        "${mod}+m" = "exec emacsclient -r";
+        "${mod}+s" = "exec wezterm start spt;exec spotifyd";
+        "Print" = ''
+          exec grim -g "$(slurp)" - | convert -  -shave 1x1 PNG: - | wl-copy'';
+        "Shift+Print" = ''exec grim -g "$(slurp)" - | swappy -f -'';
+
+        # Focus
+        "${mod}+${left}" = "focus left";
+        "${mod}+${down}" = "focus down";
+        "${mod}+${up}" = "focus up";
+        "${mod}+${right}" = "focus right";
+
+        # Move
+        "${mod}+Shift+${left}" = "move left";
+        "${mod}+Shift+${down}" = "move down";
+        "${mod}+Shift+${up}" = "move up";
+        "${mod}+Shift+${right}" = "move right";
+
+        # split
+        "${mod}+c" = "split h";
+        "${mod}+v" = "split v";
+
+        # fullscreen
+        "${mod}+f" = "fullscreen toggle";
+
+        # Other
+        "${mod}+e" = "layout toggle split";
+        "${mod}+Shift+space" = "floating toggle";
+        "${mod}+space" = "focus mode_toggle";
+        "${mod}+a" = "focus parent";
+
+        "${mod}+Shift+c" = "reload";
+
+        "${mod}+Shift+1" = "move container to workspace 1; workspace 1";
+        "${mod}+Shift+2" = "move container to workspace 2; workspace 2";
+        "${mod}+Shift+3" = "move container to workspace 3; workspace 3";
+        "${mod}+Shift+4" = "move container to workspace 4; workspace 4";
+        "${mod}+Shift+5" = "move container to workspace 5; workspace 5";
+        "${mod}+Shift+6" = "move container to workspace 6; workspace 6";
+        "${mod}+Shift+7" = "move container to workspace 7; workspace 7";
+        "${mod}+Shift+8" = "move container to workspace 8; workspace 8";
+        "${mod}+Shift+9" = "move container to workspace 9; workspace 9";
+        "${mod}+Shift+0" = "move container to workspace 10";
+
+        "XF86AudioRaiseVolume" =
+          "exec --no-startup-id pamixer -i 5 && pkill -RTMIN+12 i3blocks";
+        "XF86AudioLowerVolume" =
+          "exec --no-startup-id pamixer -d 5 && pkill -RTMIN+12 i3blocks ";
+        "XF86AudioMute" =
+          "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && pkill -RTMIN+12 i3blocks";
+        "XF86AudioMicMute" =
+          "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && $refresh_i3status";
+        "XF86MonBrightnessUp" = "exec light -A 5 && pkill -RTMIN+15 i3blocks";
+        "XF86MonBrightnessDown" =
+          "exec light -U 5 && pkill -SIGRTMIN+15 i3blocks";
+      };
+      modes = {
+        resize = {
+          Down = "resize grow height 10 px";
+          Escape = "mode default";
+          Left = "resize shrink width 10 px";
+          Return = "mode default";
+          Right = "resize grow width 10 px";
+          Up = "resize shrink height 10 px";
+          h = "resize shrink width 10 px";
+          j = "resize grow height 10 px";
+          k = "resize shrink height 10 px";
+          l = "resize grow width 10 px";
         };
-        gaps.inner = 5;
+      };
 
-        focus.followMouse = false;
-
-        keybindings = lib.mkOptionDefault {
-          "${mod}+q" = "kill";
-          "${mod}+b" = "exec firefox-beta";
-          "${mod}+d" = "exec wofi --show drun";
-          "${mod}+t" = "exec wezterm";
-          "${mod}+m" = "exec emacsclient -r";
-          "${mod}+s" = "exec wezterm start spt;exec spotifyd";
-          "Print" = "exec grim -g \"$(slurp)\" - | convert -  -shave 1x1 PNG: - | wl-copy";
-          "Shift+Print" = "exec grim -g \"$(slurp)\" - | swappy -f -";
-
-
-          # Focus
-          "${mod}+${left}" = "focus left";
-          "${mod}+${down}" = "focus down";
-          "${mod}+${up}" = "focus up";
-          "${mod}+${right}" = "focus right";
-
-          # Move
-          "${mod}+Shift+${left}" = "move left";
-          "${mod}+Shift+${down}" = "move down";
-          "${mod}+Shift+${up}" = "move up";
-          "${mod}+Shift+${right}" = "move right";
-
-          # split
-          "${mod}+c" = "split h";
-          "${mod}+v" = "split v";
-
-          # fullscreen
-          "${mod}+f" = "fullscreen toggle";
-
-          # Other
-          "${mod}+e" = "layout toggle split";
-          "${mod}+Shift+space" = "floating toggle";
-          "${mod}+space" = "focus mode_toggle";
-          "${mod}+a" = "focus parent";
-
-          "${mod}+Shift+c" = "reload";
-
-          "${mod}+Shift+1" = "move container to workspace 1; workspace 1";
-          "${mod}+Shift+2" = "move container to workspace 2; workspace 2";
-          "${mod}+Shift+3" = "move container to workspace 3; workspace 3";
-          "${mod}+Shift+4" = "move container to workspace 4; workspace 4";
-          "${mod}+Shift+5" = "move container to workspace 5; workspace 5";
-          "${mod}+Shift+6" = "move container to workspace 6; workspace 6";
-          "${mod}+Shift+7" = "move container to workspace 7; workspace 7";
-          "${mod}+Shift+8" = "move container to workspace 8; workspace 8";
-          "${mod}+Shift+9" = "move container to workspace 9; workspace 9";
-          "${mod}+Shift+0" = "move container to workspace 10";
-
-          "XF86AudioRaiseVolume" = "exec --no-startup-id pamixer -i 5 && pkill -RTMIN+12 i3blocks";
-          "XF86AudioLowerVolume" = "exec --no-startup-id pamixer -d 5 && pkill -RTMIN+12 i3blocks ";
-          "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && pkill -RTMIN+12 i3blocks";
-          "XF86AudioMicMute" = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle && $refresh_i3status";
-          "XF86MonBrightnessUp" = "exec light -A 5 && pkill -RTMIN+15 i3blocks";
-          "XF86MonBrightnessDown" = "exec light -U 5 && pkill -SIGRTMIN+15 i3blocks";
+      bars = [ ];
+      colors = {
+        focused = {
+          background = focused;
+          border = focused;
+          childBorder = focused;
+          indicator = focused;
+          text = base;
         };
-        modes = {
-          resize = {
-            Down = "resize grow height 10 px";
-            Escape = "mode default";
-            Left = "resize shrink width 10 px";
-            Return = "mode default";
-            Right = "resize grow width 10 px";
-            Up = "resize shrink height 10 px";
-            h = "resize shrink width 10 px";
-            j = "resize grow height 10 px";
-            k = "resize shrink height 10 px";
-            l = "resize grow width 10 px";
-          };
+        unfocused = {
+          background = unfocused;
+          border = unfocused;
+          childBorder = unfocused;
+          indicator = unfocused;
+          text = base;
         };
-
-        bars = [
-        ];
-        colors = {
-          focused = {
-            background = focused;
-            border = focused;
-            childBorder = focused;
-            indicator = focused;
-            text = base;
-          };
-          unfocused = {
-            background = unfocused;
-            border = unfocused;
-            childBorder = unfocused;
-            indicator = unfocused;
-            text = base;
-          };
-          focusedInactive = {
-            background = focused_inactive;
-            border = focused_inactive;
-            childBorder = focused_inactive;
-            indicator = focused_inactive;
-            text = base;
-          };
-          urgent = {
-            background = urgent;
-            border = urgent;
-            childBorder = urgent;
-            indicator = urgent;
-            text = urgent;
-          };
+        focusedInactive = {
+          background = focused_inactive;
+          border = focused_inactive;
+          childBorder = focused_inactive;
+          indicator = focused_inactive;
+          text = base;
+        };
+        urgent = {
+          background = urgent;
+          border = urgent;
+          childBorder = urgent;
+          indicator = urgent;
+          text = urgent;
         };
       };
     };
+  };
 }
