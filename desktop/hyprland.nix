00760c70616500efeefea1a09e9c89511a9081b7
JGirCo <juanmanuelgiraldoc@gmail.com>
2024-05-07 19:23:47 -0500

    hyprland config WIP

--- desktop/hyprland.nix
+++ desktop/hyprland.nix
@@ -31,9 +31,65 @@ in with colors; {
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      border_size = 4;
-      gaps_in = 4;
-      col.active_border = focused;
+      "$mod" = "${mod}";
+      general = {
+        border_size = 4;
+        gaps_in = 4;
+        "col.active_border" = focused;
+      };
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+        "$mod ALT, mouse:272, resizewindow"
+      ];
+      input = {
+        name = "keyboard";
+        kb_layout = "latam";
+        touchpad = { natural_scroll = true; };
+
+        decoration = {
+          rounding = 10;
+          drop_shadow = false;
+        };
+
+        animations = {
+          enabled = true;
+
+          bezier = "myBezier, 0.25, 0.9, 0.1, 1.02";
+          animation = [
+            "windows, 1, 7, myBezier"
+            "windowsMove, 1, 7, myBezier"
+            "windowsOut, 1, 7, default, popin 80%"
+            "border, 1, 10, default"
+            "borderangle, 1, 8, default"
+            "fade, 1, 7, default"
+          ];
+        };
+
+        dwindle = {
+          # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+          pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+          preserve_split = true; # you probably want this
+        };
+
+        master = {
+          # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+          new_is_master = true;
+          # soon :)
+          # orientation = "center";
+        };
+
+        gestures = {
+          # See https://wiki.hyprland.org/Configuring/Variables/ for more
+          workspace_swipe = false;
+        };
+
+        bind = [
+          "$mod, T, exec, wezterm"
+          "$mod, Q, killactive"
+          "$mod, SHIFT, SPACE"
+        ];
+      };
     };
   };
 }
