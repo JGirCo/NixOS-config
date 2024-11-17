889d880b88f5b1a8f0db9b6a4920d75035c68039
JGirCo <juanmanuelgiraldoc@gmail.com>
2024-11-16 22:07:25 -0500

    nvidia config [broken]

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,51 @@ flake-overlays:
     ./hardware-configuration.nix
   ];
 
+  hardware = {
+    bluetooth.enable = true; # enables support for Bluetooth
+    bluetooth.powerOnBoot =
+      true; # powers up the default Bluetooth controller on boot
+    graphics.enable = true;
+    pulseaudio.enable = false;
+    nvidia = {
+
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      prime = {
+        # sync.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+
   # Experimental features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -30,11 +75,7 @@ flake-overlays:
 
   # Enable bluetooth
 
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
   services.blueman.enable = true;
-  hardware.bluetooth.powerOnBoot =
-    true; # powers up the default Bluetooth controller on boot
-  hardware.graphics.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Bogota";
@@ -57,6 +98,7 @@ flake-overlays:
   # Enable the X11 windowing system.
   security.polkit.enable = true;
   services.xserver.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
   services.xserver.excludePackages = [ pkgs.xterm ];
 
   services.xserver.displayManager.lightdm.enable = true;
@@ -85,7 +127,6 @@ flake-overlays:
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -173,6 +214,7 @@ flake-overlays:
     ncdu
 
     # GUI Tools
+    pavucontrol
     ripdrag
     nsxiv
     spot
