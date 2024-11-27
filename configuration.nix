65af9b93cc3ed642aa4fc786d8117050dbaa39cc
JGirCo <juanmanuelgiraldoc@gmail.com>
2024-11-27 16:19:07 -0500

    Working config!!!!!! :D

--- configuration.nix
+++ configuration.nix
@@ -22,43 +22,43 @@ in {
     bluetooth.powerOnBoot =
       true; # powers up the default Bluetooth controller on boot
     graphics.enable = true;
-    # nvidia = {
-    #
-    #   # Modesetting is required.
-    #   modesetting.enable = true;
-    #
-    #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    #   # Enable this if you have graphical corruption issues or application crashes after waking
-    #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    #   # of just the bare essentials.
-    #   powerManagement.enable = false;
-    #
-    #   # Fine-grained power management. Turns off GPU when not in use.
-    #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    #   powerManagement.finegrained = false;
-    #
-    #   # Use the NVidia open source kernel module (not to be confused with the
-    #   # independent third-party "nouveau" open source driver).
-    #   # Support is limited to the Turing and later architectures. Full list of
-    #   # supported GPUs is at:
-    #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    #   # Only available from driver 515.43.04+
-    #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    #   open = false;
-    #
-    #   # Enable the Nvidia settings menu,
-    #   # accessible via `nvidia-settings`.
-    #   nvidiaSettings = true;
-    #
-    #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-    #
-    #   prime = {
-    #     sync.enable = true;
-    #     intelBusId = "PCI:0:2:0";
-    #     nvidiaBusId = "PCI:1:0:0";
-    #   };
-    # };
+    nvidia = {
+
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      prime = {
+        sync.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
   };
 
   programs.hyprland = { enable = true; };
@@ -105,10 +105,10 @@ in {
   services.udisks2.enable = true;
   security.polkit.enable = true;
   services.xserver.enable = true;
-  # services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
   services.xserver.excludePackages = [ pkgs.xterm ];
 
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
 
   # Enable i3wm
   # services.xserver.windowManager.i3 = {
