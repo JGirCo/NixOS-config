6ad72e7281a863581ee4caf9aea8e3122b9bbd91
Juan Manuel Giraldo <juanmanuelgiraldoc@gmail.com>
2025-10-30 12:14:34 -0500

    Added zen

--- apps/zen.nix
+++ apps/zen.nix
@@ -1,16 +1,42 @@
-{ inputs, pkgs, ... }: {
+{ inputs, pkgs, theme, lib, ... }:
+
+let
+  colors = import ../colors.nix {
+    inherit theme;
+    inherit lib;
+  };
+in {
   imports = [
     # inputs.zen-browser.homeModules.beta
     # or inputs.zen-browser.homeModules.twilight
     inputs.zen-browser.homeModules.twilight-official
   ];
 
-  programs.zen-browser.nativeMessagingHosts =
-    [ pkgs.firefoxpwa pkgs.tridactyl-native ];
-  profiles."default" = {
+  programs.zen-browser.enable = true;
+  programs.zen-browser.nativeMessagingHosts = [ pkgs.tridactyl-native ];
+  programs.zen-browser.profiles."default" = {
     name = "default";
-    extraConfig = ''
-      user_pref("mod.sameerasw.zen_transparency_color", "#");
-    ''
+    preConfig = ''
+      user_pref("mod.sameerasw.zen_transparency_color", "#${colors.base}C0");
+    '';
+    isDefault = true;
   };
+
+  programs.zen-browser.profiles."Whatsapp" = {
+    name = "Whatsapp";
+    id = 1;
+    preConfig = ''
+      user_pref("mod.sameerasw.zen_transparency_color", "#${colors.base}C0");
+    '';
+    isDefault = false;
+  };
+  # programs.firefox.enable = true;
+  # programs.firefox.profiles."test" = {
+  #   name = "test";
+  #   preConfig = ''
+  #     user_pref("mod.sameerasw.zen_transparency_color", "#00f1ffC0");
+  #   '';
+  #   isDefault = true;
+  #   search.default = "dgg";
+  # };
 }
